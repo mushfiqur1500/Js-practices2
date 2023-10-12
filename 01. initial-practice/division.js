b3c529433be0aa451261d5d94e6a93471a853f31
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2023-10-12 19:59:14 +0600

    first practice

---
+++ 01. initial-practice/division.js
@@ -0,0 +1,5 @@
+const division=(x, y)=>{
+    return x/y;
+}
+
+console.log('division is: ', division(40, 5))
---
+++ 01. initial-practice/index.js
@@ -0,0 +1,8 @@
+console.log('-----starting new project');
+
+const sum=(x,y)=>{
+    return x+y
+}
+
+console.log(`-----sum is ${sum(6,8)}`);
+
