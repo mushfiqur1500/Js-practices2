309f6f5710b0baa5a701e0b850c3014ec41488c9
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-09-03 13:44:27 +0600

    added object task1-4

---
+++ object/task4.js
@@ -0,0 +1,13 @@
+// Count the number of properties.
+
+// Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let studentProperty=Object.keys(student);
+console.log(studentProperty.length);
+//  output 4
