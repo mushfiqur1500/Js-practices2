2070c50045857a569be48885175580867f32a347
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-09-04 22:06:21 +0600

    sorting number in array PS -task1

--- Problem-Solution-LWS/task1.js
+++ Problem-Solution-LWS/task1.js
@@ -2,3 +2,8 @@
 const arrayName=["mitu","sonia","jerin","nabil","zahid"]
 const sortArray= arrayName.sort();
 console.log(sortArray)
+
+
+// sort the array value numerically
+const arrayNumber=[12,15,6,3,16,9,34,5,2,1]
+console.log(arrayNumber.sort(function(a,b){return a-b}))
