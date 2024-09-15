5791025081f3a65066721f16891a259eb5d2edde
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-09-15 20:11:38 +0600

    gryattribute

--- DOM/practice/stylein.js
+++ DOM/practice/stylein.js
@@ -9,20 +9,20 @@ for (const loop of section) {
 // change the background color of section2
 const customize1 = document.getElementById("section2");
 customize1.style.backgroundColor = "red";
-console.log(customize1.innerHTML);
+// console.log(customize1.innerHTML);
 
 // fruit class inner text print
 const fruit = document.getElementsByClassName("fruit");
 for (const loop of fruit) {
-  console.log(loop.innerText);
+  // console.log(loop.innerText);
 }
 
 // specific one id style
 const book1= document.getElementById('book1')
 book1.style.color="orange"
-console.log(`book 1 number is ${book1.innerText}`)
+// console.log(`book 1 number is ${book1.innerText}`)
 
 // console section1 structure, change the background color , change some style also
 const section1=document.getElementById('section1')
-console.log(section1.innerHTML)
+// console.log(section1.innerHTML)
 section1.style.backgroundColor="purple"
